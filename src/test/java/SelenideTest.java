7d3d50725fcbbfcf777cc7ef7fc69d92bed7b6e7
Aslan.K.Kardanov <Aslan.K.kardanov@rt.ru>
2024-09-08 01:34:13 +0300

    added_02

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,3 +1,26 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
 public class SelenideTest {
+    @Test
+    public  void testIssueSearch(){
+        open("https://github.com/");
+        $("qbsearch-input[class='search-input']").click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").pressEnter();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#80")).should(exist);
+
+
+    }
+
 
 }
